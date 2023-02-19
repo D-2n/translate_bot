3af08cb4810dbc9eb6f0ef686f6af158f299c5c7
Dimitrije Zdrale <114923905+D-2n@users.noreply.github.com>
2023-02-19 21:33:47 +0100

    i was cooking

--- app.py
+++ app.py
@@ -1,4 +1,3 @@
-from flask import Flask, request
 import telegram.ext as tp
 from telegram.ext import *
 import os
@@ -10,7 +9,6 @@ from googletrans import Translator
 TOKEN = os.getenv("BOT_TOKEN")
 URL = os.getenv("URL")
 bot = telegram.Bot(token=TOKEN)
-app = Flask(__name__)
 
 updater = tp.Updater(TOKEN,use_context=True)
 disp = updater.dispatcher
@@ -22,48 +20,185 @@ group_name = []
 admins = ["yeetermeister"]
 nfsb=0
 
-@app.route("/{}".format(TOKEN), methods=["POST"])
-def respond():
-    # retrieve the message in JSON and then transform it to Telegram object
-    update = telegram.Update.de_json(request.get_json(force=True), bot)
-    chat_id = update.message.chat.id
-    msg_id = update.message.message_id
+def listen_for_user(bot_token, chat_id, user_id, chat_id2, nfsb):
+    url = f"https://api.telegram.org/bot{TOKEN}/getUpdates?offset=-1"
+    lastid = None
+    response = requests.get(url)
+    updates = json.loads(response.text)
+    upd = updates["result"][-1]
+    if "message" in upd:
+        if upd["update_id"] != lastid:
+            lastid = upd["update_id"]
+            if upd["message"]["chat"]["type"] == 'private':
+                korisnik = upd["message"]["from"]["username"]
+                if korisnik in admins:
+                    if nfsb == 0:
+                        nfsb = 1
+                    else:
+                        first = upd["message"]["text"].split(' ')[0]
+                        # ADD ADMIN------------------------------------
+                        if first == "/add_admin":
+                            id = upd["message"]["chat"]["id"]
+                            second = upd["message"]["text"].split(' ')[1]
+                            if second in admins:
+                                send_fdb(bot_token, id, "Already there!")
+                            else:
+                                admins.append(second)
+                                send_fdb(bot_token, id, "Admin added!")
+                        # REMOVE ADMIN -------------------------------
+                        if first == "/remove_admin":
+                            id = upd["message"]["chat"]["id"]
+                            second = upd["message"]["text"].split(' ')[1]
+                            if second in admins:
+                                admins.remove(second)
+                                send_fdb(bot_token, id, "Admin removed!")
+                            else:
+                                send_fdb(bot_token, id, "Wasn't admin!")
+                        # ADD SOURCE --------------------------------------
+                        if first == '/add_source':
+                            id = upd["message"]["chat"]["id"]
+                            title = upd["message"]["text"].split(' ')[1]
+                            if title in group_name:
+                                send_fdb(bot_token, id, "Source already added!")
+                            else:
+                                group_name.append(title)
+                                send_fdb(bot_token, id, "Source added!")
+                        # REMOVE SOURCE-------------------------------------
+                        if first == '/remove_source':
+                            id = upd["message"]["chat"]["id"]
+                            title = upd["message"]["text"].split(' ')[1]
+                            if title in group_name:
+                                group_name.remove(title)
+                                send_fdb(bot_token, id, "Source removed!")
+                            else:
+                                send_fdb(bot_token, id, "Source wasn't added!")
+                        # ADD LISTEN ---------------------------------------------
+                        if first == '/add_listen':
+                            id = upd["message"]["chat"]["id"]
+                            second = upd["message"]["text"].split(' ')[1]
+                            if second in user_id:
+                                send_fdb(bot_token, id, "User already added!")
+                            else:
+                                user_id.append(second)
+                                send_fdb(bot_token, id, "User added!")
+                        # REMOVE LISTEN--------------------------------------------------------
+                        if first == '/remove_listen':
+                            id = upd["message"]["chat"]["id"]
+                            second = upd["message"]["text"].split(' ')[1]
+                            if second in user_id:
+                                user_id.remove(second)
+                                send_fdb(bot_token, id, "User removed!")
+                            else:
+                                send_fdb(bot_token, id, "User wasn't added!")
+                                print(user_id)
+            tip = upd["message"]["chat"]["type"]
+            if (tip in ['group', 'supergroup']):
+                korisnik = upd["message"]["from"]["username"]
+                if korisnik in user_id:
+                    if nfsb == 0:
+                        nfsb = 1
+                    else:
+                        first = upd["message"]["text"].split(' ')[0]
+                        if korisnik in admins:
+                            if first == '/set_out':
+                                id = upd["message"]["chat"]["id"]
+                                second = upd["message"]["text"].split(' ')[1]
+                                if id in chat_id_check:
+                                    send_fdb(bot_token, id, "Chat already added to output list!")
+                                else:
+                                    chat_id2.append((id, second))
+                                    chat_id_check.append(id)
+                                    send_fdb(bot_token, id, "Chat successfully added to output list!")
+                            # REMOVE FROM OUTPUT LIST -------------------------------------------------
+                            if first == '/remove_out':
+                                id = upd["message"]["chat"]["id"]
+                                if id in chat_id_check:
+                                    ind = chat_id_check.index(id)
+                                    chat_id_check.remove(chat_id_check[ind])
+                                    chat_id2.remove(chat_id2[ind])
+                                    send_fdb(bot_token, id, "Chat removed from output list!")
+                                else:
+                                    send_fdb(bot_token, id, "Chat wasn't added!")
 
-    # Telegram understands UTF-8, so encode text for unicode compatibility
-    text = update.message.text.encode("utf-8").decode()
+                        naziv = upd["message"]["chat"]["title"]
+                        if naziv in group_name:
+                            if "text" in upd["message"]:
+                                text = upd["message"]["text"]
+                                send_message(bot_token, chat_id2, format(text))
+                            else:
+                                if "photo" in upd["message"]:
+                                    if nfsb == 0:
+                                        nfsb = 1
+                                    else:
+                                        if "caption" in upd["message"]:
+                                            cap = upd["message"]["caption"]
+                                            pv = upd["message"]["photo"][0]["file_id"]
+                                            send_pv(bot_token, chat_id2, pv, format(cap))
+                                        else:
+                                            cap = ''
+                                            pv = upd["message"]["photo"][0]["file_id"]
+                                            send_pv(bot_token, chat_id2, pv, format(cap))
 
-    # the first time you chat with the bot AKA the welcoming message
-    if text == "/start":
-        # print the welcoming message
-        bot_welcome = "Hi! I respond by echoing messages. Give it a try!"
-        # send the welcoming message
-        bot.sendMessage(chat_id=chat_id, text=bot_welcome, reply_to_message_id=msg_id)
-
-    else:
-        bot.sendMessage(chat_id=chat_id, text=text)
 
     return "ok"
+# ----------------------------------------------------------------------
+def translate(text, lang):
+    strale = Translator()
+    prevod = strale.translate(text, dest=lang)
+    return prevod.text
 
 
-@app.route("/setwebhook", methods=["GET", "POST"])
-def set_webhook():
-    # we use the bot object to link the bot to our app which live
-    # in the link provided by URL
-    s = bot.setWebhook("{URL}{HOOK}".format(URL=URL, HOOK=TOKEN))
-    # something to let us know things work
-    if s:
-        return "webhook setup ok"
-    else:
-        return "webhook setup failed"
+# ----------------------------------------------------------------------
+def send_fdb(bot_token, id, text):
+    url = "https://api.telegram.org/bot{}/sendMessage".format(bot_token)
+    params = {
+        "chat_id": id,
+        "text": text
+    }
+    response = requests.post(url, params=params)
 
 
-@app.route("/")
-def index():
-    return "Hello, welcome to the telegram bot index page"
+def send_message(bot_token, chat_id2, text):
+    url = "https://api.telegram.org/bot{}/sendMessage".format(bot_token)
+    for chat_id in chat_id2:
+        newtxt = translate("TRANSLATED AUTOMATICALLY!\n" + text, chat_id[1])
+        params = {
+            "chat_id": chat_id[0],
+            "text": newtxt
+        }
+        response = requests.post(url, params=params)
+
+
+def send_pv(bot_token, to_chat, pv, txt):
+    url = "https://api.telegram.org/bot{}/sendPhoto".format(bot_token)
+    for chat_id in to_chat:
+        newtxt = translate("TRANSLATED AUTOMATICALLY!\n" + txt, chat_id[1])
+        params = {
+            "chat_id": chat_id[0],
+            "photo": str(pv),
+            "caption": newtxt
+        }
+        response = requests.post(url, params=params)
+
+
+
+
 
 
 if __name__ == "__main__":
     # note the threaded arg which allow
     # your app to have more than one thread
-    app.run(threaded=True)
+    s = bot.setWebhook("{URL}{HOOK}".format(URL=URL, HOOK=TOKEN))
+    # something to let us know things work
+    if s:
+        print ("webhook setup ok")
+    else:
+        print("webhook setup failed")
+    bot_token = TOKEN
+    updater = tp.Updater(TOKEN, use_context=True)
+    disp = updater.dispatcher
+    # listen
+    listen_for_user(bot_token, chat_id, user_id, chat_id2, nfsb)
+updater.start_polling(0.5)
+updater.idle()
 
